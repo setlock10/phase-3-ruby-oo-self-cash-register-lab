902dde686de800fed5eba2b824275ac21b756020
John J Setlock II <johnsetlock@gmail.com>
2022-08-31 08:27:19 -0500

    commit tree 93e633af488b4c1a36aff8ab11bb6829242fe620

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+require 'pry'
+
+class CashRegister 
+    attr_accessor :discount, :total, :items
+
+    def initialize (discount=0)
+        @discount =discount
+        @total=0
+        @items=[]
+
+
+    end
+
+    def add_item (title,price,quantity=1)
+        # @total = @total+ ((price*(100-@discount)*quantity)/100)
+        @total = @total+ (price*quantity)
+        i=0
+        while i<quantity
+            @items<<title
+            i+=1
+        end
+
+
+    end
+
+    def apply_discount
+        if discount>0
+            @total=@total-(@total*(@discount.to_f/100))
+            return "After the discount, the total comes to $#{@total.to_i}."
+        else
+            return "There is no discount to apply."
+        end
+     end
+
+
+
+end
+
+
+binding.pry
 
